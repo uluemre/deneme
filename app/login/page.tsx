73f9785fd482f36aaa72f24ea01c0f99023b90fa
BunyaminTeke <tekebunyamin06@gmail.com>
2025-07-04 16:27:02 +0300

    Style sorunu düzeltildi

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-// import styles from './login.module.css';
 import './login.css'
 
 import InputField from '../../components/login/InputFields'
 
 
 function Login() {
+
     return (
 
-
-        <div>
+        <div className='wrapper'>
             <InputField />
         </div>
     );
